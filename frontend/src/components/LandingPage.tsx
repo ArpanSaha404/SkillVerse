f339fd668f77aa9c8394ad3c40fd550374b9072f
ArpanSaha404 <arpan50saha@gmail.com>
2024-11-15 23:07:34 +0530

    corrected CSS

--- frontend/src/components/LandingPage.tsx
+++ frontend/src/components/LandingPage.tsx
@@ -1,12 +1,8 @@
-import axios from "axios";
 import { Button } from "./ui/button";
-import { frontend_URL_dev } from "./lib/utils";
-import { useState } from "react";
+import React, { useState } from "react";
 import { MoveRight } from "lucide-react";
 
 const LandingPage = () => {
-  const isLoading = false;
-
   const [SearchText, setSearchText] = useState("");
 
   const changeInputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -14,58 +10,41 @@ const LandingPage = () => {
     setSearchText(e.target.value);
   };
 
-  const login = {
-    email: "1stUser@gmail.com",
-    password: "1stUser",
-  };
-  const handleClick = async () => {
-    await axios
-      .post(`${frontend_URL_dev}/api/users/login`, login)
-      .then((res) => {
-        if (res.data) {
-          console.log(res.data);
-          alert(res.data.apiMsg);
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-
-  if (isLoading) {
-    return (
-      <div className="bg-red-600">
-        LandingPage
-        <Button onClick={handleClick}>Button</Button>
-      </div>
-    );
-  }
+  const handleSearchClick = () => {};
 
   return (
     <div className="h-screen md:divCenter">
-      <div className="mx-8 w-6/12 font-sans font-bold flex justify-center items-center">
+      <div className="ml-8 mr-4 w-6/12 font-sans font-bold flex justify-center items-center">
         <div className="space-y-8">
           <h1 className="text-6xl">
             Improve Your Online Learning Experience Better Instantly
           </h1>
+          <div className="hidden">We have 30k+ Courses...</div>
           <div className="space-x-4 flex justify-start items-center">
             <input
-              className="border-black border-2 border-solid rounded-md px-8 h-10 w-3/4"
+              className="border-black border-2 border-solid rounded-md pl-4 h-10 w-3/4"
               name="serachText"
               type="text"
               placeholder="Search for a Course or Category"
               value={SearchText}
               onChange={changeInputHandler}
+              onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) => {
+                if (e.key === "Enter") {
+                  handleSearchClick();
+                }
+              }}
             />
-            <Button className="h-10 hover:bg-gray-600">
+            <Button
+              className="h-10 hover:bg-gray-600"
+              onClick={handleSearchClick}
+            >
               <MoveRight />
             </Button>
+            <div className="hidden">We have 30k+ Courses...</div>
           </div>
         </div>
-        <div className="hidden">We have 30k+ Courses...</div>
-        <div className="hidden">We have 30k+ Courses...</div>
       </div>
-      <div className="divCenter w-6/12">
+      <div className="divCenter w-6/12 mr-8 ml-4">
         <img
           style={{ borderRadius: "50%" }}
           src="https://images.pexels.com/photos/247819/pexels-photo-247819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
